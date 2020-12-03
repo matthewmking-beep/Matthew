4c558cb9645803755994082770c5baf859a26cd7
Matthew King <matthewmking@cmail.carleton.ca>
2020-12-02 20:11:16 -0500

    Small Edits

--- Text-Based Adventure/Text-Based Adventure/Text-Based-Adventure.h
+++ Text-Based Adventure/Text-Based Adventure/Text-Based-Adventure.h
@@ -3,6 +3,8 @@
 #include <string>
 #include "Character.h"
 #include "Environment.h"
+#include "Item.h"
+#include "Quest.h"
 
 
 //Variables
@@ -18,6 +20,12 @@ std::string fightingCommandList[5];
 //Environment Array
 EnvironmentObject environmentArray[5];
 int currentEnvironment;
+
+//Player Character
+characterObject Player;
+
+//Monster Array
+characterObject monsterList[10];
 
 //Functions
 void initialSetup();
